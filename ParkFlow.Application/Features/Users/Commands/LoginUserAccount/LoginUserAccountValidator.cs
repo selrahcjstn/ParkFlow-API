9c2732b8c430cf63ec1f5a40d2606c1b2eb92f3f
Charles Justine Mantes <charlesjustinemantes.main@gmail.com>
2026-05-10 12:55:10 +0800

    Refactor login user account validation by consolidating into a single validator class and removing the obsolete validator file

--- ParkFlow.Application/Features/Users/Commands/LoginUserAccount/LoginUserAccountValidator.cs
+++ ParkFlow.Application/Features/Users/Commands/LoginUserAccount/LoginUserAccountValidator.cs
@@ -1,8 +1,19 @@
-using System;
+using FluentValidation;
 
 namespace ParkFlow.Application.Features.Users.Commands.LoginUserAccount;
 
-public class LoginUserValidator
+public class LoginUserAccountValidator : AbstractValidator<LoginUserAccountCommand>
 {
+    public LoginUserAccountValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.")
+            .MaximumLength(256);
 
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Invalid email or password.")
+            .MaximumLength(100);
+    }
 }
