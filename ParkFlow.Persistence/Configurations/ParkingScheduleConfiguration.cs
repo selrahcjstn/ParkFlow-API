58cbd8787e46972efd06d93030b6a647dc7951cd
Charles Justine Mantes <charlesjustinemantes.main@gmail.com>
2026-05-11 10:21:37 +0800

    feat: Implement ParkingSchedule entity configuration with required properties and relationships

--- ParkFlow.Persistence/Configurations/ParkingScheduleConfiguration.cs
+++ ParkFlow.Persistence/Configurations/ParkingScheduleConfiguration.cs
@@ -1,8 +1,36 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ParkFlow.Domain.Entities;
 
 namespace ParkFlow.Persistence.Configurations;
 
-public class ParkingScheduleConfiguration
+public class ParkingScheduleConfiguration : IEntityTypeConfiguration<ParkingSchedule>
 {
+	public void Configure(EntityTypeBuilder<ParkingSchedule> entity)
+	{
+		entity.HasKey(e => e.Id);
 
+		entity.Property(e => e.Id)
+			.HasDefaultValueSql("gen_random_uuid()");
+
+		entity.Property(e => e.CreatedAt)
+			.HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+		entity.Property(e => e.DayOfWeek)
+			.IsRequired();
+
+		entity.Property(e => e.StartTime)
+			.IsRequired();
+
+		entity.Property(e => e.EndTime)
+			.IsRequired();
+
+		entity.HasOne(e => e.CorSubmission)
+			.WithMany()
+			.HasForeignKey(e => e.SubmissionId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		entity.HasIndex(e => e.SubmissionId);
+	}
 }
