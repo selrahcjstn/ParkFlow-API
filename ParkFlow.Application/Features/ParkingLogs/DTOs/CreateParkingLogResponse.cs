b684df968a5e5e9b48ed4adee3e53dc42206a580
Charles Justine Mantes <charlesjustinemantes.main@gmail.com>
2026-05-19 15:33:43 +0800

    Refactor parking log handlers and responses
    Removed unused properties (`IdNumber`, `Course`, `YearLevel`, `Section`, `Department`) from handlers and responses. Added `PhoneNumber` property to improve data handling. Removed `MaximumExitTime` from `CreateParkingLogResponse`.

--- ParkFlow.Application/Features/ParkingLogs/DTOs/CreateParkingLogResponse.cs
+++ ParkFlow.Application/Features/ParkingLogs/DTOs/CreateParkingLogResponse.cs
@@ -6,19 +6,11 @@ public class CreateParkingLogResponse
     public string LastName { get; set; } = null!;
     public string Role { get; set; } = null!;
     public string Status { get; set; } = null!;
-
-    public string IdNumber { get; set; } = null!;
-    public string? Course { get; set; }
-    public int? YearLevel { get; set; }
-    public string? Section { get; set; }
-    public string? Department { get; set; }
-
+    public string PhoneNumber { get; set; } = null!;
     public string PlateNumber { get; set; } = null!;
     public string Brand { get; set; } = null!;
     public string VehicleType { get; set; } = null!;
-
     public DateTime EntryTime { get; set; }
     public DateTime EntryDate { get; set; }
-
     public DateTime? MaximumExitTime { get; set; }
 }
--- ParkFlow.Application/Features/ParkingLogs/DTOs/ExitParkingLogResponse.cs
+++ ParkFlow.Application/Features/ParkingLogs/DTOs/ExitParkingLogResponse.cs
@@ -6,11 +6,6 @@ public class ExitParkingLogResponse
     public string LastName { get; set; } = null!;
     public string Role { get; set; } = null!;
     public string Status { get; set; } = null!;
-    public string IdNumber { get; set; } = null!;
-    public string? Course { get; set; }
-    public int? YearLevel { get; set; }
-    public string? Section { get; set; }
-    public string? Department { get; set; }
     public string PlateNumber { get; set; } = null!;
     public string Brand { get; set; } = null!;
     public string VehicleType { get; set; } = null!;
