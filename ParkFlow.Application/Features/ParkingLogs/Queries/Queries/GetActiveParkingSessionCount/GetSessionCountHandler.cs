3d2237dc2cfb039cf80716c9e86279e06d0211a7
Charles Justine Mantes <charlesjustinemantes.main@gmail.com>
2026-05-21 16:23:09 +0800

    refactor(vehicles): remove GetVehiclesByOwnerId functionality and related classes; reintroduce GetVehiclesByOwnerIdHandler with updated logic

--- ParkFlow.Application/Features/ParkingLogs/Queries/Queries/GetActiveParkingSessionCount/GetSessionCountHandler.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-using ParkFlow.Application.Common;
-using ParkFlow.Domain.Enums;
-
-namespace ParkFlow.Application.Features.Vehicles.Queries.GetSessionCount;
-
-public record GetSessionCountQuery(Guid OwnerId) : IRequest<Result<IEnumerable<VehicleDto>>>;
-
